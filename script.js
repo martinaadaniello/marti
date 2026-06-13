3b17dd5380d6b99bad70622649eb6dada73cb47e
martinaadaniello <martinaadaniello@icloud.com>
2026-06-13 12:30:08 +0200

    Update script.js

--- script.js
+++ script.js
@@ -99,17 +99,17 @@ document.addEventListener("DOMContentLoaded", () => {
 
 const buyBtn = document.getElementById("floating-buy-btn");
 
-document.addEventListener("mousemove", (e) => {
-
-  if (e.clientY >= window.innerHeight * 0.7) {
-    buyBtn.classList.add("visible");
-  } else {
-    buyBtn.classList.remove("visible");
-  }
-
-});
-
 if (buyBtn) {
+
+  document.addEventListener("mousemove", (e) => {
+
+    if (e.clientY >= window.innerHeight * 0.7) {
+      buyBtn.classList.add("visible");
+    } else {
+      buyBtn.classList.remove("visible");
+    }
+
+  });
 
   buyBtn.addEventListener("click", (e) => {
 
